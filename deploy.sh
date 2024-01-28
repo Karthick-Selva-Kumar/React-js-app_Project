1f7a7c0b229051f7e5d46ccceeba5c258fc9fae8
Karthick <151127150+Karthick-Selva-Kumar@users.noreply.github.com>
2024-01-28 19:51:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# Assuming the following environment variables are set:
-# DOCKER_REGISTRY_CREDS=<your_credentials_id>
-# DOCKER_USERNAME=<your_docker_username>
-# DOCKER_PASSWORD=<your_docker_password>
-# DOCKER_BFLASK_IMAGE=<your_docker_image>
+# Run Docker Compose
+docker-compose --down
+docker-compose up -d
 
 # Login to Docker registry
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
 
 # Push the Docker image to the registry
-docker push "$DOCKER_BFLASK_IMAGE"
+docker push karthickselvakumar1999/dev:myreactimg
 
