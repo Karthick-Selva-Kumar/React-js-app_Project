fa74f945ff8a9d74fde58808698b29607e439cfe
Karthick <151127150+Karthick-Selva-Kumar@users.noreply.github.com>
2024-01-28 20:21:48 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,9 @@ docker-compose up -d
 # Login to Docker registry
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
 
+#tagging the image:
+docker tag myreactimg karthickselvakumar1999/myreactimg:dev
+
 # Push the Docker image to the registry
-docker push karthickselvakumar1999/dev:myreactimg
+docker push karthickselvakumar1999/myreactimg:dev
 
